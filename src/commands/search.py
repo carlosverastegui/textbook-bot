d553c840055494c7613094238953fe7d481fdd5e
xomien <xomien@users.noreply.github.com>
2020-10-31 03:01:21 -0400

    Add Amazon functionality

--- src/commands/search.py
+++ src/commands/search.py
@@ -1,34 +1,24 @@
 import discord
 from integrations import *
 
+amazon = Amazon(None)
+
 
 @discord.bot.command()
 async def search(ctx, isbn: str):
-    """
-    item_details_amazon = await amazon(isbn)
-    item_details_chegg = await chegg(isbn)
-
-    item_details = item_details_amazon.update(item_details_chegg)
-
-    sorted_items = sorted(item_details.items(), key=lambda x: x["price"], reverse=True)
-
-    print(sorted_items, flush=True)
-    """
-
-
-    """
     embed = discord.Embed(title="Textbook Results")
 
-    embed.description = "Textbook name: **test**\n\n" \
-                        "1.) Amazon - $10\nLink: [click here](https://google.com)\n\n" \
-                        "2.) Chegg - $15\nLink: [click here](https://google.com)\n\n" \
-                        "3.) Pearson - $20\nLink: [click here](https://google.com)"
+    amazon_data = await amazon.search(isbn)
+
+    if amazon_data:
+        title = amazon_data["name"]
+        price = amazon_data["price"]
+        amazon_url = amazon_data["url"]
+        embed.description = f"Textbook name: **{title}**\n\n" \
+                            f"1.) Amazon - [${price}]({amazon_url})"
+
+    else:
+        embed.description = "No results found. Please double check your ISBN and try again."
+
 
     await ctx.send(embed=embed)
-
-    failed_embed = discord.Embed(title="Textbook Results")
-    failed_embed.description = "No results found. Please double-check your ISBN and try again."
-    await ctx.send(embed=failed_embed)
-    """
-
-    pass
--- src/integrations/amazon.py
+++ src/integrations/amazon.py
@@ -1,15 +1,40 @@
 import aiohttp
+import re
+
+from aiohttp.client_exceptions import ContentTypeError
+from utils import request
+
 
 
 class Amazon:
     def __init__(self, key):
         self.key = key
+        self.amazon_regex = re.compile('href="(.+)" rel="nofollow" target="_blank">Amazon</a></td><td><b><span id="p_amazon">\$(.+)</span>')
 
     async def search(self, isbn):
-        # use self.key for your API auth
-        # use with aiohttp to do the requests
+        try:
+            book_data = await request(f"https://api.itbook.store/1.0/books/{isbn}")
 
-        return {
-            "price": 500,
-            "name": "test item"
-        }
+            title = book_data.get("title")
+            authors = book_data.get("authors")
+            url = book_data.get("url")
+            isbn10 = book_data.get("isbn10")
+
+            if url:
+                book_page = await request(url, json=False)
+                amazon_search = self.amazon_regex.search(book_page)
+
+                if amazon_search:
+                    #amazon_url = f"https://itbook.store/{amazon_search.group(1)}
+                    amazon_url = f"https://www.amazon.com/gp/product/{isbn10}"
+                    price = amazon_search.group(2)
+
+                    return {
+                        "price": float(price),
+                        "name": title,
+                        "authors": authors,
+                        "url": amazon_url
+                    }
+
+        except ContentTypeError:
+            return None
