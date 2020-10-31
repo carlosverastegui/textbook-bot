887faed9685da780653f707b7cc052a143b55f12
xomien <xomien@users.noreply.github.com>
2020-10-31 03:39:05 -0400

    Use Chromium Edge User-Agent for requests

--- src/utils.py
+++ src/utils.py
@@ -6,7 +6,9 @@ def get_files(directory):
 
 async def request(url, json=True):
     async with aiohttp.ClientSession() as session:
-        async with session.get(url) as response:
+        async with session.get(url, headers={
+            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/1.1.1.1.1 Safari/537.36 Edg/1.1.1.1.1"
+        }) as response:
             if json:
                 return await response.json()
             else:
