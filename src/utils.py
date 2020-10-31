d50f4124881ccefe56b113f9d62d90e360cdc888
xomien <xomien@users.noreply.github.com>
2020-10-31 03:01:34 -0400

    Add utility HTTP request function

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,13 @@
 from os import listdir
-
+import aiohttp
 
 def get_files(directory):
     return [name for name in listdir(directory) if name[:1] != "." and name[:2] != "__" and name != "_DS_Store"]
+
+async def request(url, json=True):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as response:
+            if json:
+                return await response.json()
+            else:
+                return await response.text()
