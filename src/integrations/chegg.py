161539250d5e8d3a41b638c3600a6e234485d659
Carlos Verastegui <51972213+carlosverastegui@users.noreply.github.com>
2020-11-08 23:01:17 -0500

    Update chegg.py

--- src/integrations/chegg.py
+++ src/integrations/chegg.py
@@ -8,8 +8,16 @@ class Chegg:
     async def search(self, isbn):
         # use self.key for your API auth
         # use with aiohttp to do the requests
-
+        password = 1
+        book_data = await request(f"http://api.chegg.com/rent.svc?KEY={self.key}&PW={password}&isbn={isbn}&R=JSON&with_pids=1&page=1&results_per_page=10&V=4.0")
+        
+        book_info = book_data.get('Items')[0]
+        title = temp.get('BookInfo', {}).get('Title') 
+        price = temp.get('BookInfo', {}).get('ListPrice') 
+        authors = temp.get('BookInfo', {}).get('Authors') 
+        
         return {
-            "price": 500,
-            "name": "test item"
+            "price": price,
+            "name": title,
+            "authors": authors
         }
