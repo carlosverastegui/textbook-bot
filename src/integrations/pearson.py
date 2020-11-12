3438ccda118cc41b1707e36bf73e651113165f2e
shrug98 <39962695+shrug98@users.noreply.github.com>
2020-11-11 22:06:36 -0500

    Update pearson.py

--- src/integrations/pearson.py
+++ src/integrations/pearson.py
@@ -9,7 +9,17 @@ class Pearson:
         # use self.key for your API auth
         # use with aiohttp to do the requests
 
+        session = HTMLSession()
+        r = session.get(f"https://www.pearson.com/store/en-us/search.html?_charset_=UTF-8&q={isbn}")
+        price = r.html.find('.selected-product__price', first=True).text
+       
+
+        title = r.html.find('.product-summary__heading', first=True).text
+        author = r.html.find('.product-authors', first=True).text
+         
         return {
-            "price": 500,
-            "name": "test item"
+            "price": price,
+            "name": title,
+            "authors": author
         }
+
