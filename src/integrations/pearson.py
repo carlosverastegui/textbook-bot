84a56b9a8e436159a8a388677be983a80189cc46
shrug98 <39962695+shrug98@users.noreply.github.com>
2020-11-11 22:37:23 -0500

    Update pearson.py

--- src/integrations/pearson.py
+++ src/integrations/pearson.py
@@ -8,18 +8,21 @@ class Pearson:
     async def search(self, isbn):
         # use self.key for your API auth
         # use with aiohttp to do the requests
+        try: 
+            session = HTMLSession()
+            r = session.get(f"https://www.pearson.com/store/en-us/search.html?_charset_=UTF-8&q={isbn}")
+            price = r.html.find('.selected-product__price', first=True).text
 
-        session = HTMLSession()
-        r = session.get(f"https://www.pearson.com/store/en-us/search.html?_charset_=UTF-8&q={isbn}")
-        price = r.html.find('.selected-product__price', first=True).text
-       
 
-        title = r.html.find('.product-summary__heading', first=True).text
-        author = r.html.find('.product-authors', first=True).text
-         
-        return {
-            "price": price,
-            "name": title,
-            "authors": author
-        }
+            title = r.html.find('.product-summary__heading', first=True).text
+            author = r.html.find('.product-authors', first=True).text
+
+            return {
+                "price": price,
+                "name": title,
+                "authors": author
+            }
+        
+        except:
+            return None 
 
